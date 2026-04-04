f5b5b7c093b29c135f6b88edfed1df2bde8ca7d9
root <chamodkalupahana@gmail.com>
2026-04-04 16:22:54 +0000

    validation results

--- linear_probe/not_detected_vs_detected_correct/layer_and_position_sweep/save_activations_by_layer_and_position.py
+++ linear_probe/not_detected_vs_detected_correct/layer_and_position_sweep/save_activations_by_layer_and_position.py
@@ -1,3 +1,10 @@
-# load model
+from original_paper.compute_concept_vector import compute_vector_single_prompt
 
-# load abstract dataset
+# load model 
+
+# load abstract dataset (small)
+
+# compute concept vector
+    # using compute_vector_single_prompt
+
+# inject
